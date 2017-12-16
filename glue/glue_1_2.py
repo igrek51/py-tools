601e05d9fc1b582db8a2b94c6bf150e2a2f541bc
iszulc <iszulc@relayonit.pl>
2017-12-16 02:36:32 +0100

    glue: fix info and warn padding

--- glue/glue_1_2.py
+++ glue/glue_1_2.py
@@ -19,10 +19,10 @@ def debug(message):
     print('\033[32m\033[1m[debug]\033[0m ' + message)
 
 def info(message):
-    print('\033[34m\033[1m[ info]\033[0m ' + message)
+    print('\033[34m\033[1m[info]\033[0m  ' + message)
 
 def warn(message):
-    print('\033[33m\033[1m[ warn]\033[0m ' + message)
+    print('\033[33m\033[1m[warn]\033[0m  ' + message)
 
 def error(message):
     print('\033[31m\033[1m[ERROR]\033[0m ' + message)
