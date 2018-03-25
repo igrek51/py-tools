a4d5d8640603f849f7d1241f6f47bd0abe73b890
igrek <igrek.s@o2.pl>
2018-03-25 18:12:15 +0200

    update lichking

---
+++ lichking/autocomplete_lichking.sh
@@ -0,0 +1,17 @@
+_lichking() {
+    # COMPREPLY=()
+    # lich test duPa 51
+    # echo ""
+    # echo "COMP_WORDS : ${COMP_WORDS}" # lich
+    # echo "COMP_CWORD : ${COMP_CWORD}" # 2
+    # echo "COMP_WORDS[COMP_CWORD] : ${COMP_WORDS[COMP_CWORD]}" # duPa
+    # echo "COMP_LINE : ${COMP_LINE}" # lich test duPa 51
+    # echo "COMP_POINT : ${COMP_POINT}" # 12
+    # echo "COMP_KEY : ${COMP_KEY}" # 9
+    # echo "COMP_TYPE : ${COMP_TYPE}" # 9
+    # echo "args : $@" # lich du test
+    # echo "reply : ${COMPREPLY}" #
+    COMPREPLY=( $(./autocomplete_lichking.py --compLine "${COMP_LINE}") )
+}
+
+complete -F _lichking lichking lich king dupa
