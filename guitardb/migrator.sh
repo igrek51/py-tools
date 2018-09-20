d2118d07271262b547987cebada871229d81ffbc
igrek51 <igrek.s@o2.pl>
2018-09-20 21:37:54 +0200

    update guitar db migrator

--- guitardb/migrator.sh
+++ guitardb/migrator.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 export INPUT=/mnt/data/Igrek/guitarDB
 export OUTPUT=/mnt/data/Igrek/guitarDB/songs.sqlite
-export DB_VERSION_NUMBER=5
+export SONGBOOK_DB=/mnt/data/Igrek/android/songbook/app/src/main/res/raw/songs.sqlite
+export DB_VERSION_NUMBER=8
 
 ./guitardb_migrate.py --input $INPUT --output $OUTPUT --versionNumber $DB_VERSION_NUMBER
+
+cp ${OUTPUT} ${SONGBOOK_DB}
